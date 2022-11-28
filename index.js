aa9fd42f1e107fc1f3631dd7a4d92c943af9797a
tanveerisonline <tanveerisonline@gmail.com>
2022-11-28 16:32:40 +0530

    Add function to index.js

--- index.js
+++ index.js
@@ -7,74 +7,86 @@
 //  3. A unique id to identify the book (we can use Math.random() to generate the id)
 
 class BookInfo {
-    constructor(title, author) {
-      this.title = title;
-      this.author = author;
-      this.bookId = Math.random();
-    }
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+    this.bookId = Math.random();
   }
-  
-  // Create a class for the collection. This class will contain:
-  
-  //  a function to remove books
-  
-  class Collection {
-    //  1. a constructor for book data
-    constructor() {
-      // create an array of BookInfos
-      this.bookData = [];
-    }
-  
-    //  a function to add books
-    addBook(singleBook) {
-      // push BookInfo to bookData
-      this.bookData.push(singleBook);
-      // save it to localStorage
-      localStorage.setItem('collection', JSON.stringify(this.bookData));
-      // add to the webpage
-      addToPage(singleBook);
-    }
-  
-    deleteBook(bookId) {
-      // get the book element by id
-      const bookElement = document.getElementById(bookId);
-      bookElement.remove();
-      this.bookData = this.bookData.filter((bookObject) => bookObject.id !== bookId);
-      localStorage.setItem('collection', JSON.stringify(this.bookData));
-    }
+}
+
+// Create a class for the collection. This class will contain:
+
+//  a function to remove books
+
+class Collection {
+  //  1. a constructor for book data
+  constructor() {
+    // create an array of BookInfos
+    this.bookData = [];
   }
-  const collection = new Collection();
-  // Create a function tp get the from inputs
-  function readInput() {
-    // get book title from the input
-    const title = document.getElementById('book-title');
-    // get book title from the input
-    const author = document.getElementById('book-author');
-    // create a book object
-    const book = new BookInfo(title.value, author.value);
-    // reset the form
-    title.value = '';
-    author.value = '';
-    return book;
+
+  //  a function to add books
+  addBook(singleBook) {
+    // push BookInfo to bookData
+    this.bookData.push(singleBook);
+    // save it to localStorage
+    localStorage.setItem('collection', JSON.stringify(this.bookData));
+    // add to the webpage
+    addToPage(singleBook);
   }
-  // Create a function to add data to the page
-  
-  
-  // Code for add button
-  const addBtn = document.getElementById('add-btn');
-  addBtn.addEventListener('click', () => {
-    const singleBook = readInput();
-    collection.addBook(singleBook);
-  });
-  
-  // construct the collection ont the page using data from local storage
-  window.onload = () => {
-    collection.data = JSON.parse(localStorage.getItem('collection' || '[]'));
-    if (collection.data === null) {
-      collection.data = [];
-      return;
-    }
-  
-    collection.data.forEach((singleBook) => addToPage(singleBook));
-  };
-  
+
+  deleteBook(bookId) {
+    // get the book element by id
+    const bookElement = document.getElementById(bookId);
+    bookElement.remove();
+    this.bookData = this.bookData.filter((bookObject) => bookObject.id !== bookId);
+    localStorage.setItem('collection', JSON.stringify(this.bookData));
+  }
+}
+const collection = new Collection();
+// Create a function tp get the from inputs
+function readInput() {
+  // get book title from the input
+  const title = document.getElementById('book-title');
+  // get book title from the input
+  const author = document.getElementById('book-author');
+  // create a book object
+  const book = new BookInfo(title.value, author.value);
+  // reset the form
+  title.value = '';
+  author.value = '';
+  return book;
+}
+// Create a function to add data to the page
+function addToPage(bookObject) {
+  const bookList = document.getElementById('book-list');
+  const singleBook = document.createElement('div');
+  singleBook.setAttribute('id', bookObject.id);
+  singleBook.innerHTML = `<p>${bookObject.title}</p>
+                    <p>${bookObject.author}</p>`;
+  const deleteBtn = document.createElement('button');
+  deleteBtn.innerHTML = 'Delete';
+  deleteBtn.addEventListener('click', () => collection.deleteBook(bookObject.id));
+  const hr = document.createElement('hr');
+  singleBook.appendChild(deleteBtn);
+  singleBook.appendChild(hr);
+  bookList.appendChild(singleBook);
+}
+
+// Code for add button
+const addBtn = document.getElementById('add-btn');
+addBtn.addEventListener('click', () => {
+  const singleBook = readInput();
+  collection.addBook(singleBook);
+});
+
+// construct the collection ont the page using data from local storage
+window.onload = () => {
+  collection.data = JSON.parse(localStorage.getItem('collection' || '[]'));
+  if (collection.data === null) {
+    collection.data = [];
+    return;
+  }
+
+  collection.data.forEach((singleBook) => addToPage(singleBook));
+};
